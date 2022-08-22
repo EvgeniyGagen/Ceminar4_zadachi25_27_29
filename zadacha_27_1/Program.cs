fa6d92353f9c1f2e818124e2d88f66b968a61abb
Evgeniy <evg961@mail.ru>
2022-08-22 17:26:29 +0300

    27 1

---
+++ zadacha_27_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите чсло");
+        int ostatoc = Convert.ToInt32(Console.ReadLine());
+        int count = 0;
+        while (ostatoc > 0)
+        {
+            count = count + ostatoc % 10;
+            ostatoc = (ostatoc - ostatoc % 10) / 10;
+        }
+        Console.WriteLine("Сумма всех цифр вводимого числа" + count);
+    }
+
+}
+
