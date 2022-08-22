1ec5df723cd62152cab0d64b996ea06b4bf60796
Evgeniy <evg961@mail.ru>
2022-08-22 15:49:09 +0300

    25_1

---
+++ zadacha_25_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+static void Main(string[] args)
+{    
+Console.Clear();
+int PowerRase(int arg1, int arg2)
+{
+    if (arg2 < 0) arg2 = arg2 * (-1);
+    int result = 1;
+    for (int i =0; i < arg2; i++)
+    {
+        result = result*arg1;
+    }
+    return result;
+}
+
+int Input()
+{
+    Console.WriteLine("Input number ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int factor = Input();
+int exponent = Input();
+int degree = PowerRase(factor, exponent);
+Console.WriteLine($"{factor} возвести в степень {Math.Abs(exponent)} равно {degree}");
+    }
+}
