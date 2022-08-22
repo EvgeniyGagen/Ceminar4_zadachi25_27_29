90ed74e0b30bb973e209fd7084292b677f3127c1
Evgeniy <evg961@mail.ru>
2022-08-22 17:22:40 +0300

    29 2

---
+++ zadacha29_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+class Program
+{
+    
+    static void Main (string[] args)
+    {
+        string number = Console.ReadLine(); 
+        string[] array = new string[number.Length];
+        int index = 0;
+        foreach (var item in number) 
+        {
+            if(item != ',')
+            {
+                array[index] += item.ToString();
+               
+            }else{ 
+                index++;
+            }
+        }
+        Console.Write("[ "); 
+        for(int i = 0; i<=index-1; i++)
+        {
+            Console.Write(array[i] + ", "); 
+        }
+        Console.Write(array[index] + " ]");
+    }
+}
+
---
+++ zadacha_29_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+class Program
+{
+    
+    static void Main (string[] args)
+    {
+        int [] numbers = {1, 2, 5, 7, 19};
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            Console.Write("["+ numbers[i]+  "]"); 
+        }
+        // string number = Console.ReadLine(); 
+        // string[] array = new string[number.Length];
+        // int index = 0;
+        // foreach (var item in number) 
+        // {
+        //     if(item != ',')
+        //     {
+        //         array[index] += item.ToString();
+               
+        //     }else{ 
+        //         index++;
+        //     }
+        // }
+        // Console.Write("[ "); 
+        // for(int i = 0; i<=index-1; i++)
+        // {
+        //     Console.Write(array[i] + ", "); 
+        // }
+        // Console.Write(array[index] + " ]");
+    }
+}
