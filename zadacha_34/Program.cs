b9e43ca0d30595562e4d129d2493303c3fb65cbe
Evgeniy <evg961@mail.ru>
2022-08-24 17:10:00 +0300

    zadacha 34

---
+++ zadacha_34/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+
+
+    int [] numbers = new int[10];
+    void Array1 (int[] array, int min, int max)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[i] - new Random().Next(min, max);
+        }
+    }
+    void Array2(int[] array)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]+ " ");
+        }
+        Console.WriteLine();
+    }
+
+    int evenInt(int[] array)
+    {
+        int amount = 0;
+        for(int i = 0; i < array.Length; i++)
+        {
+            if(array[i] % 2 == 1)
+            {
+                amount++;
+            }
+        }return amount;
+    }
+    Array1(numbers, 100, 1000);
+    Array2(numbers);
+    Console.WriteLine();
+    int amount = evenInt(numbers);
+    Console.WriteLine($" Кол-во четных чисел в массиве: {amount}");
+
