136468546d74025572a22d1dc568547e1afc9766
Evgeniy <evg961@mail.ru>
2022-08-24 19:16:27 +0300

    zadacha 36

---
+++ zadacha_36/Program.cs
@@ -0,0 +1,12 @@
+﻿int array = new int[10];
+int suma = 0;
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 10);
+    Console.Write(array[i]+ " ");
+    if(i%2 == 1)
+    {
+        suma += array[i];
+    }
+}
+Console.WriteLine($" {suma}");
